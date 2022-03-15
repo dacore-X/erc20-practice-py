2cffce5e463a25e5827056510d71952d05de67ed
xKarmalware <romanchik129@gmail.com>
2022-03-15 03:25:24 +0300

    - add tests for transfer function

--- tests/basic_calls.py
+++ tests/basic_calls.py
@@ -1,11 +1,8 @@
-import configparser
-
 from erc20token import ERC20Token
 from web3 import Web3, HTTPProvider
 from util import _load_cfg
-from types_eth import AddressLike
+from types_eth import AddressLike, ChecksumAddress
 from typing import Union
-from pprint import pprint
 
 
 PROVIDER = _load_cfg()["WEB3"]["PROVIDER"]
@@ -14,7 +11,10 @@ w3 = Web3(HTTPProvider(PROVIDER))
 TEST_TOKENS = [
     # KOVAN TOKENS
     '0x13512979ADE267AB5100878E2e0f485B568328a4',  # USDT
-    '0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa',  # DAI
+    '0xe22da380ee6B445bb8273C81944ADEB6E8450422',  # USDC
+    # '0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa',  # DAI
+    '0xFf795577d9AC8bD7D90Ee22b6C1703490b6512FD',  # DAI AUGMENTED
+    '0xa36085F69e2889c224210F603D836748e7dC0088',  # LINK
     # '0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984',  # UNI ERROR! METAMASK ASK TO SIGN
     '0xB347b9f5B56b431B2CF4e1d90a5995f7519ca792',  # POLYMATH
     '0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f',  # SNX
@@ -32,7 +32,7 @@ TEST_TOKENS = [
 def test_basic_call(token: Union[AddressLike, str]):
     token = ERC20Token(token, w3)
     token.set_all_options()
-    print(token)
+    # print(token)
 
 
 def test_approve_token(token: Union[AddressLike, str]):
@@ -43,6 +43,16 @@ def test_approve_token(token: Union[AddressLike, str]):
     # print(response)
 
 
+def test_transfer_token(token: Union[AddressLike, str], to: Union[AddressLike, str], amount: float):
+    token = ERC20Token(token, w3)
+    to = Web3.toChecksumAddress(to)
+    response = token.transfer(to, amount)
+    # print(response)
+
+
 for TOKEN in TEST_TOKENS:
-    test_basic_call(TOKEN)
-    test_approve_token(TOKEN)
+    # test_basic_call(TOKEN)
+    # test_approve_token(TOKEN)
+    # test_transfer_token(TOKEN, '0x0b209139eaddbe99ffcd2a08cb45b92aca902833', 100)
+    pass
+
